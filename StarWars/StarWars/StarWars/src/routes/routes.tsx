f7dda83510b02dad11d2e7add5a28745e32e5c2e
Milda100 <157160973+Milda100@users.noreply.github.com>
2025-06-29 00:04:06 +0300

    Deploying StarWars project

--- StarWars/StarWars/StarWars/src/routes/routes.tsx
+++
@@ -1,6 +0,0 @@
-export const ROUTES = {
-  movies: "/movies",
-  movieDetail: (id: string) => `/movies/${id}`,
-  characters: "/characters",
-  characterDetail: (id: string) => `/characters/${id}`,
-};
--- src/utils/helper.tsx
+++
@@ -1,4 +0,0 @@
-export const extractIdFromUrl = (url: string): string => {
-  const match = url.match(/\/(\d+)\/?$/); // Notice the optional trailing slash
-  return match ? match[1] : '';
-};
